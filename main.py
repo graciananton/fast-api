e0ebcb73b7413bbc56d43b17f3341a8b10c08a7c
Gracian Anton <gracian.anton@gmail.com>
2026-04-29 11:44:11 -0400

    commit

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from datetime import datetime, timezone, timedelta
+from zlib import crc32
+import matplotlib.pyplot as plt
+import joblib
+from scipy.stats import randint
+
+from bs4 import BeautifulSoup
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import RandomizedSearchCV
+
+import numpy as np
+import pandas as pd
+
+import requests
+import json
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "API is running"}
+@app.get("/train_model")
+def train_model():
+    return get_stations()
+
+def get_stations():
+    stations_url = "https://gracian.ca/laravel/public/api/stations"
+    res = requests.get(stations_url)
+    stations = res.json()
+    return stations
