944cd11e92866001fa8b96ed22eb9b3f9cc3d224
Gracian Anton <gracian.anton@gmail.com>
2026-04-29 12:57:25 -0400

    commit

--- ml/utils.py
+++ ml/utils.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime, timezone, timedelta
 
 load_dotenv() 
 
@@ -11,3 +12,8 @@ def get_stations():
     stations = res.json()
     return stations
 
+def days_ago(days:int):
+    time_now = datetime.now(timezone.utc)
+    time_ago = time_now - timedelta(days = days)
+    formatted_time_ago = time_ago.strftime("%Y-%m-%d %H:%M:%S.%f")
+    return formatted_time_ago
